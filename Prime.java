7fa3d90cb3de62f59e258a826dacffbb14e97f48
ssablaise007 <75985793+ssablaise007@users.noreply.github.com>
2021-07-17 22:18:06 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,12 +1,41 @@
-package LogicalPrograms;
-import Utility.java.Utility;
-public class Prime {
+package Utility.java;
+import java.util.Scanner;
 
-		public static void main(String[] args) {
-			Utility utility = new Utility();
-			System.out.print("Enter the num to check prime or not : ");
-			int n = utility.getIntValue();
-			utility.Prime(n);
-		}
+/*
+ * *********************** Utility Class ****************************
+ */
+public class Utility {
+	Scanner scanner;
+	
+	public Utility() {
+		/*
+		 * Scanner class instance for scanner 
+		 */
+		scanner = new Scanner(System.in);
+	}
+/*
+ * input function to take integer from user
+ */
+	public int getIntValue() {
+		return scanner.nextInt();
 	}
 
+public void Prime(int num) {
+	boolean flag = true;{
+		if(num != 0 && num != 1) {
+			
+			for(int i=2; i<=num/2; i++) {
+				if(num % i == 0)
+					flag = false;
+			}
+			if(flag)
+				System.out.println(num+" is prime number");
+			else
+				System.out.println(num+" is not prime number");
+		}
+		else
+			System.out.println(num+" is not prime number");
+	}
+	}
+}
+	
